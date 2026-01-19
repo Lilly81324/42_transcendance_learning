725fe077a924482575ad2c5915c9823135cfb6aa
Lilly Kunne <simkue2@web.de>
2026-01-19 19:47:35 +0100

    Undid AI tests and added more text

--- Preparation/BabylonJS/test1/script.js
+++ Preparation/BabylonJS/test1/script.js
@@ -1,28 +1,12 @@
-export function createScene(engine, canvas) {
-  const scene = new BABYLON.Scene(engine);
+export const createScene = function () {
+    
+    const scene = new BABYLON.Scene(engine);
 
-  const camera = new BABYLON.ArcRotateCamera(
-    "camera",
-    -Math.PI / 2,
-    Math.PI / 2.5,
-    15,
-    BABYLON.Vector3.Zero(),
-    scene
-  );
-  camera.attachControl(canvas, true);
-
-  new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(1, 1, 0),
-    scene
-  );
-
-  BABYLON.SceneLoader.ImportMeshAsync(
-    "",
-    "./objects/",
-    "Dimond.glb",
-    scene
-  );
-
-  return scene;
-}
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+    
+    BABYLON.ImportMeshAsync("./objects/Dimond.glb");
+    
+    return scene;
+};
