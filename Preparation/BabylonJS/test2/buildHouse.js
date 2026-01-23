578e95729dbf22e55fbb26e76cfac32e6c8ac6eb
Lilly Kunne <simkue2@web.de>
2026-01-23 16:47:22 +0100

    Fixed texture mapping of house and roof

--- Preparation/BabylonJS/test2/buildHouse.js
+++ Preparation/BabylonJS/test2/buildHouse.js
@@ -6,8 +6,12 @@ function buildRoof(position, scene)
 	// Create Texture for Base
 	material.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/textures/crate.png", scene);
 
+	// Map Texture to Roof
+	const uvMap = [];
+	uvMap[0] = new BABYLON.Vector4(0.5, 0, 1, 1);
+
 	// Create Roof
-	const roof = new BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.5, height: 1.0, tessellation: 3, faceUV: uvMap, wrap: true});
+	const roof = new BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.5, height: 1.0, tessellation: 3, faceUV: uvMap, warp: true});
 	roof.scaling.x = 0.6;
 	roof.rotation.z = Math.PI / 2;
     roof.position.x = position.x;
@@ -31,8 +35,17 @@ function buildBase(position, scene)
 	// Create Texture for Base
 	material.diffuseTexture = new BABYLON.Texture("textures/violentdays_large.jpg", scene);
 
+	// Map Texture for Base
+	let uvMap = [];
+	uvMap[0] = new BABYLON.Vector4(0,		(1/3),	0.25,	(2/3));
+	uvMap[1] = new BABYLON.Vector4(0.25,	(1/3),	0.5,	(2/3));
+	uvMap[2] = new BABYLON.Vector4(0.5,		(1/3),	0.75,	(2/3));
+	uvMap[3] = new BABYLON.Vector4(0.75,	(1/3),	1.0,	(2/3));
+	uvMap[4] = new BABYLON.Vector4(0.25,	(2/3),	0.5,	 1);
+	uvMap[5] = new BABYLON.Vector4(0.25,	0,		0.5,	 (1/3));
+
 	// Create Box with given Mapping and Material
-	const box = new BABYLON.MeshBuilder.CreateBox("box", scene)
+	const box = new BABYLON.MeshBuilder.CreateBox("box", {faceUV: uvMap, warp: true}, scene)
 	box.material = material;
 	return (box);
 }
