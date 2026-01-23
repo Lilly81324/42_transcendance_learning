0ba84d291baf1b84e6c1abe93dbe2da65702793a
Lilly Kunne <simkue2@web.de>
2026-01-23 19:04:23 +0100

    Experimenting with Skybox Textures

--- Preparation/BabylonJS/test2/createSky.js
+++ Preparation/BabylonJS/test2/createSky.js
@@ -1,19 +1,10 @@
 export function createSky(scene)
 {
-	// UV-Wrap
-	let uvMap = [];
-	uvMap[0] = new BABYLON.Vector4(0,		(1/3),	0.25,	(2/3));
-	uvMap[1] = new BABYLON.Vector4(0.25,	(1/3),	0.5,	(2/3));
-	uvMap[2] = new BABYLON.Vector4(0.5,		(1/3),	0.75,	(2/3));
-	uvMap[3] = new BABYLON.Vector4(0.75,	(1/3),	1.0,	(2/3));
-	uvMap[4] = new BABYLON.Vector4(0.25,	(2/3),	0.5,	 1);
-	uvMap[5] = new BABYLON.Vector4(0.25,	0,		0.5,	 (1/3));
-
 	// Skybox
-	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {faceUV: uvMap, wrap: true, size:100.0}, scene);
+	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:100.0}, scene);
 	var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
 	skyboxMaterial.backFaceCulling = false;
-	skyboxMaterial.reflectionTexture = new BABYLON.Texture("textures/violentdays_large.jpg", scene);
+	skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/violentdays_large", scene);
 	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
 	skyboxMaterial.disableLighting = true;
 	skybox.material = skyboxMaterial;		
