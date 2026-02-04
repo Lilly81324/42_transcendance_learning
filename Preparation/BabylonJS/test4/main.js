7b2a73287af239b7722930edbb132ae9e17e56a8
Lilly K. Home <likunne@DESKTOP-QBDTI2D.localdomain>
2026-02-04 15:51:38 +0100

    Added simple Physic test

--- Preparation/BabylonJS/test4/main.js
+++ Preparation/BabylonJS/test4/main.js
@@ -6,6 +6,19 @@ const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engi
 // Add your code here matching the playground format
 const scene = createScene(engine, canvas); //Call the createScene function
 
+// Add Havoc Phys Engine and Gravity to scene
+const havokInstance = await HavokPhysics();
+const hk = new BABYLON.HavokPlugin(true, havokInstance);
+scene.enablePhysics(new BABYLON.Vector3(0, -9.8, 0), hk);
+
+      // Give Ground Map Box Colisions
+      const sphereAggregate = new BABYLON.PhysicsAggregate(scene.meshes[1], BABYLON.PhysicsShapeType.BOX, { mass: 1, restitution: 0.75 }, scene);
+
+      // Ground just in case
+      const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 12, height: 12}, scene);
+      ground.position.y = -1.5;
+      const groundAggregate = new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.BOX, { mass: 0 }, scene);
+
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
 	scene.render();
