71e57be3d4a1690e5693558f4065ab14de8f797b
Lilly Kunne <simkue2@web.de>
2026-02-06 18:21:36 +0100

    Fixed only checking position, and not position.y

--- Preparation/BabylonJS/test4/running/gameWin.js
+++ Preparation/BabylonJS/test4/running/gameWin.js
@@ -7,7 +7,7 @@ export function gameWin(scene, playerMesh)
 {
 	let won = false;
 	scene.onBeforeRenderObservable.add(() => {
-		if (!won && playerMesh.position < 3)
+		if (!won && playerMesh.position.y < 3)
 		{
 			alert("You won!");
 			won = true;
