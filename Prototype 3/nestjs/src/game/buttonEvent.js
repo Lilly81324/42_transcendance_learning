48466c52b5491fa028dfc2b3165dae457a7e6ec5
Lilly Kunne <simkue2@web.de>
2026-02-23 19:09:05 +0100

    Made Ball move up and down

--- Prototype 3/nestjs/src/game/buttonEvent.js
+++ Prototype 3/nestjs/src/game/buttonEvent.js
@@ -1,8 +1,23 @@
 export function buttonEvent(socket, scene)
 {
+	let up = true;
 	socket.on("msgToClient", (data) => {
 		console.log(`Message from server ${data}`);
 		const ref = scene.getMeshByName("sphere");
-		ref.position.y = ref.position.y + 1;
+		const pos = ref.position.y;
+		if (pos <= 1)
+			up = true;
+		else if (pos >= 3)
+			up = false;
+		if (up)
+			ref.position.y = pos + 0.5;
+		else
+			ref.position.y = pos - 0.5;
 	});
 }
+
+
+// d
+// ud
+// ud
+// u
