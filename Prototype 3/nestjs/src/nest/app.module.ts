a8dec2a2e8a6e742c64bff96d8bc75c5d92a1095
Lilly Kunne <simkue2@web.de>
2026-02-23 14:49:31 +0100

    Fixed Container net name not being lowercase, fixed wronginclusion of outdated EventModules

--- Prototype 3/nestjs/src/nest/app.module.ts
+++ Prototype 3/nestjs/src/nest/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { EventsModule } from './events/events.module';
 import { GamefilesModule } from './gamefiles/gamefiles.module';
 import { GameModule } from './game/game.module';
 import { HealthModule } from './health/health.module';
 import { EventsGateway } from './websock/events.gateway';
 
 @Module({
-  imports: [EventsModule, GamefilesModule, GameModule, HealthModule],
+  imports: [GamefilesModule, GameModule, HealthModule],
   providers: [EventsGateway]
 })
 export class AppModule {}
