aae69506316e6556f664e9b105bd099165918362
Lilly K. Home <likunne@DESKTOP-QBDTI2D.localdomain>
2026-03-10 04:56:17 +0100

    Fixed Bug that happened when starting count inside explosion

--- TerrainDestruction.js
+++ TerrainDestruction.js
@@ -9,7 +9,7 @@ import Vector from "./Vector.js";
  * @param radius number representing size of explosion
  * @returns Array of 0-2 Vectors pointing to Intersection points of explosion and point1->2 vector 
  */
-function checkIntersection(point1, point2, exploPoint, radius)
+function checkIntersection(point1, point2, exploPoint, radius, tolerance)
 {
 	// Calculate helper values
 	const V = Vector.sub(point2, point1);
@@ -20,16 +20,25 @@ function checkIntersection(point1, point2, exploPoint, radius)
 	const b = Vector.dotproduct(V, D) * 2;
 	const c = Vector.dotproduct(D, D) - radius * radius;
 
-	// Insert into midnight formula
-	const result1 = (-b + Math.sqrt(b*b - 4*a*c))/(2*a);
-	const result2 = (-b - Math.sqrt(b*b - 4*a*c))/(2*a);
+	const discriminant = b * b -4 * a * c;
 
-	// If more or less then 0-1 * vector, then its not in range
+	if (discriminant < 0) return [];
+
+	// Insert into midnight formula
+	const sqrtofDic = Math.sqrt(b*b - 4 * a * c)
+
+	const fact1 = (-b + sqrtofDic)/(2 * a);
+	const fact2 = (-b - sqrtofDic)/(2 * a);
+
 	const results = [];
-	if (result1 >= 0 && result1 <= 1)
-		results.push(Vector.mult(V, result1).add(point1));
-	if (result2 >= 0 && result2 <= 1)
-		results.push(Vector.mult(V, result2).add(point1));
+	// Generously provided by AI
+	const sortedFactors = [fact1, fact2].sort((x, y) => x - y);
+	for (let factor of sortedFactors) {
+		if (factor >= -tolerance && factor <= 1 + tolerance) {
+			const clampedFactor = Math.max(0, Math.min(1, factor));
+			results.push(Vector.mult(V, clampedFactor).add(point1));
+		}
+	}
 	return (results);
 }
 
@@ -66,53 +75,56 @@ function onExitingExplo(newArray, exitPosition) {
 /**
  * @param araray Array of Babylon.Vector3 representing the structure of the Game Grid
  * @warning When reffering to the Arrays non-x coordinate, you need to use the z coordinate
+ * @note Needs special behaviour, if we start moving along edges inside the explosion
  */
 function redoArray(array, explosionX, explosionY, radius) {
 	const explosionPoint = new Vector(explosionX, explosionY);
-	const newArray = [];
-	let insideExplosion = insideExploCheck(array[0].x, array[0].y, explosionPoint, radius)
-	// Go through all Vectors that make up the map
-	for (let i = 0; i < array.length; i++) {
-		const point1 = new Vector(array[i].x, array[i].z);
-		var point2;
-		if (i + 1 >= array.length)
-			point2 = new Vector(array[0].x, array[0].z);
-		else
-			point2 = new Vector(array[i + 1].x, array[i + 1].z);
-		const intersectPoints = checkIntersection(point1, point2, explosionPoint, radius);
-		// Needs special case, in case we start inside the explosion
-		if (insideExplosion) {
-			if (intersectPoints.length == 0)
-				continue ;
-			else if (intersectPoints.length == 1) {
-				// exiting
-				onExitingExplo(newArray, intersectPoints[0]);
-				insideExplosion = false;
-				console.log("Case 1 - Exiting - " + point1.x + "/" + point1.y);
-			}
-			else {
-				console.log("ERROR: Terrain Manipulation Logic failed. Straight line somehow crosses circle 3 times.");
-				console.log("Case 2 - Exiting Entering - " + point1.x + "/" + point1.y);
-				return (array);
-			}
+	const len = array.length;
+	let startIndex = -1;
+	
+	// Find point that is outside the explosion to keep walking through array
+	for (let i = 0; i < len; i++) {
+		if (!insideExploCheck(array[i].x, array[i].z, explosionPoint, radius)) {
+			startIndex = i;
+			break ;
 		}
-		else {
-			if (intersectPoints.length == 0) {
-				newArray.push(new BABYLON.Vector3(point1.x, 0, point1.y));
-				console.log("Case 3 - Passing - " + point1.x + "/" + point1.y);
-			}
-			else if (intersectPoints.length == 1) {
+	}
+	
+	// Explosion consumes entire mesh
+	if (startIndex == -1) {
+		return [];
+	}
+	
+	const newArray = [];
+	let insideExplosion = false;
+
+	// Go through all Vectors that make up the map
+	for (let i = 0; i < len; i++) {
+		const currentIndex = (startIndex + i) % len;
+		const nextIndex = (startIndex + i + 1) % len;
+
+		const point1 = new Vector(array[currentIndex].x, array[currentIndex].z);
+		const point2 = new Vector(array[nextIndex].x, array[nextIndex].z);
+		console.log("Going through points: P1: " + point1.x + ",", + point1.y + " P2: " + point2.x + ", ", + point2.y);
+		const intersectPoints = checkIntersection(point1, point2, explosionPoint, radius, 0.00001);
+
+		if (!insideExplosion) {
+			newArray.push(new BABYLON.Vector3(point1.x, 0, point1.y));
+			if (intersectPoints.length == 1) {
+				console.log("Entering");
 				insideExplosion = true;
-				newArray.push(new BABYLON.Vector3(point1.x, 0, point1.y));
 				newArray.push(new BABYLON.Vector3(intersectPoints[0].x, 0, intersectPoints[0].y));
-				console.log("Case 4 - Entering - " + point1.x + "/" + point1.y);
 			}
 			else {
-				newArray.push(new BABYLON.Vector3(point1.x, 0, point1.y));
+				console.log("Entering and Exiting");
 				newArray.push(new BABYLON.Vector3(intersectPoints[0].x, 0, intersectPoints[0].y));
 				onExitingExplo(newArray, intersectPoints[1]);
-				console.log("Case 5 - Entering Exiting - " + point1.x + "/" + point1.y);
 			}
+		}
+		else if (intersectPoints.length == 1) {
+				console.log("Exiting");
+				insideExplosion = false;
+				onExitingExplo(newArray, intersectPoints[0]);
 		}
 	}
 	return (newArray);
